95a1b79c18e6ae3f3cd0a57164778489d7a16fd8
onureozcan <onureozcan@gmail.com>
2019-06-07 11:03:23 +0300

    making ignored dist and newlib dirs on demand

--- build_scripts/build_iso_using_docker.sh
+++ build_scripts/build_iso_using_docker.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+mkdir -p ../dist; # makes ignored directory
 cd ../docker/build_iso_env && docker-compose build && docker-compose up && cd ../.. && ./run_qemu.sh
--- build_scripts/build_newlib_using_docker.sh
+++ build_scripts/build_newlib_using_docker.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+mkdir -p ../newlib; # makes ignored directory
 cd ../docker/build_newlib_env && docker-compose build && docker-compose up
