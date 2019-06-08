12487aa05ba3cf0d79b96bdd2aa70389748e25a9
onureozcan <onureozcan@gmail.com>
2019-06-08 15:16:13 +0300

    some random printing

--- src/user/hello_world/hello.c
+++ src/user/hello_world/hello.c
@@ -4,6 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    printf("hello world! This is a user app speaking through write syscall!\n");
+int test(int i) {
+    return i;
+}
+
+int main(int argc, char **argv) {
+    printf("hello world! This is a user app speaking through write syscall!. %d\n", 4);
+    printf("some other thing...\n");
+    for (int i = 0; i < 50; i++) {
+        printf("test %d\n", test(i));
+    }
+    printf("apparently printf works just fine\n");
+    printf("what about args? argc: %d, argv: %p\n", argc, argv);
 }
