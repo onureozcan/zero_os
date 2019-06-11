a0849f07d7b588ccc3e5c164ead30aa11fa23b90
onureozcan <onureozcan@gmail.com>
2019-06-12 01:15:03 +0300

    memcpy and memset for kernel

--- src/kernel/lib/include/string/k_string.h
+++ src/kernel/lib/include/string/k_string.h
@@ -8,6 +8,8 @@
 
 void* memcpy(void *dest, const void *src, size_t size);
 
+void *memset(void *b, int c, size_t len);
+
 #define ZEROOS_STRING_H
 
 #endif //ZEROOS_STRING_H
--- src/kernel/lib/str/k_string.c
+++ src/kernel/lib/str/k_string.c
@@ -2,14 +2,30 @@
 // Created by onureozcan on 01.06.2019.
 //
 #include <stddef.h>
+#include <stdint-gcc.h>
 
-void *memcpy(void *dest, const void *src, size_t size) {
-    const char *src_as_char = src;
-    char *dest_as_char = dest;
-    // the most naive implementation.
-    // hope compiler optimizes it
-    for (size_t i = 0; i < size; i++) {
-        dest_as_char[i] = src_as_char[i];
+void *memcpy(void *dest, void *src, size_t n) {
+    uint32_t num_dwords = n / 4;
+    uint32_t num_bytes = n % 4;
+    uint32_t *dest32 = (uint32_t *) dest;
+    uint32_t *src32 = (uint32_t *) src;
+    uint8_t *dest8 = ((uint8_t *) dest) + num_dwords * 4;
+    uint8_t *src8 = ((uint8_t *) src) + num_dwords * 4;
+    uint32_t i;
+
+    for (i = 0; i < num_dwords; i++) {
+        dest32[i] = src32[i];
+    }
+    for (i = 0; i < num_bytes; i++) {
+        dest8[i] = src8[i];
     }
     return dest;
+}
+
+void *memset(void *b, int c, size_t len) {
+    char *s = b;
+
+    while (len--)
+        *s++ = c;
+    return b;
 }
