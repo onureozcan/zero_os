78aa2fda54c5fe73a01db6d3d38a2bc7f8762db8
onureozcan <onureozcan@gmail.com>
2019-08-04 15:25:59 +0300

    mouse cursor drawing with triangles

--- src/user/window_manager/src/screen_objects/mouse_object.c
+++ src/user/window_manager/src/screen_objects/mouse_object.c
@@ -7,33 +7,45 @@
 #include <screen_object.h>
 #include <canvas.h>
 
-#define MOUSE_DRAWING_COORD_WIDTH 150
+#define MOUSE_DRAWING_COORD_WIDTH 100
+#define MOUSE_DRAWING_COORD_HEIGHT 150
 #define SCALE_MOUSE_DRAWING_COORD_X(x) ((x) * canvas.width / MOUSE_DRAWING_COORD_WIDTH)
-#define SCALE_MOUSE_DRAWING_COORD_Y(x) ((x) * canvas.height / MOUSE_DRAWING_COORD_WIDTH)
+#define SCALE_MOUSE_DRAWING_COORD_Y(x) ((x) * canvas.height / MOUSE_DRAWING_COORD_HEIGHT)
 
 static uint8_t color = 255;
 static canvas_t canvas;
-static int line_thickness = 1;
+
+static void mouse_object_draw_mouse_cursor(uint8_t color, uint8_t alpha);
 
 int mouse_shape_coords[][2] = {
-        {26,18},
-        {26,100},
-        {27,19},
-        {88,79},
-        {88,79},
-        {50,79},
-        {50,79},
-        {26,100}
+        {0, 0},
+        {0,                         MOUSE_DRAWING_COORD_HEIGHT * 2 / 3 },
+        {MOUSE_DRAWING_COORD_WIDTH / 3, MOUSE_DRAWING_COORD_HEIGHT / 2},
+        {0, 0},
+        {MOUSE_DRAWING_COORD_WIDTH / 3, MOUSE_DRAWING_COORD_HEIGHT / 2},
+        {MOUSE_DRAWING_COORD_WIDTH, MOUSE_DRAWING_COORD_HEIGHT / 2}
 };
 
 static void repaint(screen_object_t *object) {
 
     CLEAR_DIRTY(object);
-    for(int i = 0; i < 8; i+=2) {
-        canvas_draw_line(&canvas, SCALE_MOUSE_DRAWING_COORD_X(mouse_shape_coords[i][0]), SCALE_MOUSE_DRAWING_COORD_Y(mouse_shape_coords[i][1]),
-                         SCALE_MOUSE_DRAWING_COORD_X(mouse_shape_coords[i+1][0]), SCALE_MOUSE_DRAWING_COORD_Y(mouse_shape_coords[i+1][1]), color, color, color,
-                         line_thickness);
+    mouse_object_draw_mouse_cursor(color, 50);
+}
+
+static void mouse_object_draw_mouse_cursor(uint8_t color, uint8_t alpha) {
+    int coords[6];
+    // first half
+    for (int i = 0; i < 6; i += 2) {
+        coords[i] = SCALE_MOUSE_DRAWING_COORD_X(((int *) mouse_shape_coords)[i]);
+        coords[i + 1] = SCALE_MOUSE_DRAWING_COORD_Y(((int *) mouse_shape_coords)[i + 1]);
     }
+    canvas_fill_triangle(&canvas, coords, color, color, color, alpha);
+    // second half
+    for (int i = 0; i < 6; i += 2) {
+        coords[i] = SCALE_MOUSE_DRAWING_COORD_X(((int *) mouse_shape_coords)[i + 6]);
+        coords[i + 1] = SCALE_MOUSE_DRAWING_COORD_Y(((int *) mouse_shape_coords)[i + 7]);
+    }
+    canvas_fill_triangle(&canvas, coords, color, color, color, alpha);
 }
 
 
